5cc6bf025fbc96bfcf2666ecfe80f66ff3981909
mhmdfrhn <frhn2707@gmail.com>
2023-05-16 16:30:58 +0700

    final modified

---
+++ hapi-web-server/index.js
@@ -0,0 +1,16 @@
+import Hapi from "@hapi/hapi";
+import { routes } from "./routes.js";
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: "localhost",
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log("Server running on %s", server.info.uri);
+};
+
+init();
